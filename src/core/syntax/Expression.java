c23d3f82afe2970fa08fce641cfbcb8b064b4601
MrTonyNeon <mr.tony.chulakov@gmail.com>
2024-10-08 22:44:49 +0300

    Fix

--- src/core/syntax/Expression.java
+++ src/core/syntax/Expression.java
@@ -3,13 +3,13 @@ package src.core.syntax;
 public class Expression implements ExpressionElement{
     // TODO: Should be not an ArrayList instead should be one of Literals/Punctuation/Operator
 
-    public final ExpressionElement expressions;
+    public final ExpressionElement expression;
 
-    public Expression(ExpressionElement expressions) {
-        this.expressions = expressions;
+    public Expression(ExpressionElement expression) {
+        this.expression = expression;
     }
 
-    public ExpressionElement getExpressions() {
-        return expressions;
+    public ExpressionElement getExpression() {
+        return expression;
     }
 }
