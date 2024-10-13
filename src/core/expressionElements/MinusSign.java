3cb8c0fa6af07bf0cf86d11b3d06d9db23b10b0d
MrTonyNeon <mr.tony.chulakov@gmail.com>
2024-10-14 00:06:55 +0300

    Expression Elements

---
+++ src/core/expressionElements/MinusSign.java
@@ -0,0 +1,9 @@
+package src.core.expressionElements;
+
+import src.core.Span;
+
+public class MinusSign extends ExpressionElementClass{
+    public MinusSign(String value, Span span) {
+        super(value, span);
+    }
+}
---
+++ src/core/expressionElements/PlusSign.java
@@ -0,0 +1,9 @@
+package src.core.expressionElements;
+
+import src.core.Span;
+
+public class PlusSign extends ExpressionElementClass{
+    public PlusSign(String value, Span span) {
+        super(value, span);
+    }
+}
