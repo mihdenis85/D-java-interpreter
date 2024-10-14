21af9a6957fbfe9907adf4bdc083c991ecbdcf78
mihdenis85 <mihdenis777@yandex.ru>
2024-10-14 22:32:29 +0300

    Fixed

--- src/core/syntax/interfaces/JSONConvertable.java
+++ src/core/syntax/interfaces/JSONConvertable.java
@@ -1,7 +1,6 @@
 package src.core.syntax.interfaces;
 
 import java.util.List;
-import java.util.Map;
 
 public interface JSONConvertable {
     String toJSONString();
@@ -15,19 +14,6 @@ public interface JSONConvertable {
         if (!list.isEmpty())
             sb.deleteCharAt(sb.length() - 1);
         sb.append(']');
-        return sb.toString();
-    }
-
-    static String mapToJsonString(Map<? extends JSONConvertable, ? extends JSONConvertable> map) {
-        StringBuilder sb = new StringBuilder();
-        sb.append('{');
-        for (JSONConvertable j : map.keySet()) {
-            sb.append(j.toJSONString());
-            sb.append(',');
-        }
-        if (!map.isEmpty())
-            sb.deleteCharAt(sb.length() - 1);
-        sb.append('}');
         return sb.toString();
     }
 }
