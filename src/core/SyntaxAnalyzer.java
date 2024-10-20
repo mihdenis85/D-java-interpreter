59b68f08e6fd602e408a69a4a299483e87ee413e
mihdenis85 <mihdenis777@yandex.ru>
2024-10-20 16:58:57 +0300

    Fixed after merge

--- src/core/SyntaxAnalyzer.java
+++ src/core/SyntaxAnalyzer.java
@@ -352,21 +352,22 @@ public class SyntaxAnalyzer {
                 continue;
             }
             ArrayList<TupleElement> elements = tuple.getElements();
+            Token nextToken = peekToken(1);
+            Identifier identifier = new Identifier("", nextToken.span);
 
-            Identifier identifier = expectIdentifier();
+            if (token.type == Code.tkIdentifier) {
+                identifier = expectIdentifier();
+            }
 
-            Token nextToken = peekToken(0);
             if (nextToken.type == Code.tkComma) {
-                skipToken();
-                ExpressionElement element = new EmptyLiteral(token.span);
-                Expression expression = new Expression(new ArrayList<>());
-                expression.expressions.add(element);
+                Expression expression = parseExpression();
                 TupleElement tupleElement = new TupleElement(identifier, expression);
                 elements.add(tupleElement);
                 tuple.setElements(elements);
                 token = peekToken(0);
                 continue;
             }
+
 
             matchPunct(Code.tkAssignment);
 
