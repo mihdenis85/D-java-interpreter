8ad294b2a3b6e7d93936f01dc587c60ed547aef0
mihdenis85 <mihdenis777@yandex.ru>
2024-12-01 00:51:23 +0300

    Added commented testing

--- src/core/Main.java
+++ src/core/Main.java
@@ -10,7 +10,7 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        boolean isTest = true;
+        boolean isTest = false;
         if (isTest) {
             test();
         } else {
@@ -18,7 +18,20 @@ public class Main {
             InputStream inputStream = new FileInputStream(source);
 
             LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(inputStream);
+
             ArrayList<Token> tokens = lexicalAnalyzer.getTokens();
+//
+//            for (int i = 0; i < tokens.size(); i++) {
+//                Token token = tokens.get(i);
+//                System.out.println("Type: " + token.getType());
+//                System.out.println("Value: " + token.value);
+//                System.out.println("Id: " + i);
+//                System.out.print("Line number: " + token.span.lineNum + " ");
+//                System.out.print("Position begin: " + token.span.posBegin + " ");
+//                System.out.print("Position end: " + token.span.posEnd + " ");
+//                System.out.println();
+//                System.out.println();
+//            }
 
             SyntaxAnalyzer syntaxAnalyzer = new SyntaxAnalyzer(tokens);
             Program program = syntaxAnalyzer.analyze();
@@ -26,33 +39,33 @@ public class Main {
             SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer(program.getProgram());
             Program updatedProgram = semanticAnalyzer.analyze();
 
-            try {
-                String result = updatedProgram.serializeToJson().replaceAll("\n", "").replaceAll("\t", "");
-
-                StringBuilder stringBuilder = new StringBuilder();
-                int tabNumber = 0;
-                for (int i = 0; i < result.length(); i++) {
-                    char element = result.charAt(i);
-                    if (element == '}' || element == ']') {
-                        stringBuilder.append('\n');
-                        tabNumber--;
-                        stringBuilder.append("\t".repeat(Math.max(0, tabNumber)));
-                    }
-                    stringBuilder.append(element);
-                    if (element == '{' || element == '[') {
-                        stringBuilder.append('\n');
-                        tabNumber++;
-                        stringBuilder.append("\t".repeat(Math.max(0, tabNumber)));
-                    }
-                    if (element == ',') {
-                        stringBuilder.append('\n');
-                        stringBuilder.append("\t".repeat(Math.max(0, tabNumber)));
-                    }
-                }
-                System.out.println(stringBuilder.toString());
-            } catch (Exception e) {
-                System.exit(0);
-            }
+//            try {
+//                String result = updatedProgram.serializeToJson().replaceAll("\n", "").replaceAll("\t", "");
+//
+//                StringBuilder stringBuilder = new StringBuilder();
+//                int tabNumber = 0;
+//                for (int i = 0; i < result.length(); i++) {
+//                    char element = result.charAt(i);
+//                    if (element == '}' || element == ']') {
+//                        stringBuilder.append('\n');
+//                        tabNumber--;
+//                        stringBuilder.append("\t".repeat(Math.max(0, tabNumber)));
+//                    }
+//                    stringBuilder.append(element);
+//                    if (element == '{' || element == '[') {
+//                        stringBuilder.append('\n');
+//                        tabNumber++;
+//                        stringBuilder.append("\t".repeat(Math.max(0, tabNumber)));
+//                    }
+//                    if (element == ',') {
+//                        stringBuilder.append('\n');
+//                        stringBuilder.append("\t".repeat(Math.max(0, tabNumber)));
+//                    }
+//                }
+//                System.out.println(stringBuilder.toString());
+//            } catch (Exception e) {
+//                System.exit(0);
+//            }
 
             Interpreter interpreter = new Interpreter(updatedProgram.getProgram());
             interpreter.interpret();
